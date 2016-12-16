192a62ad0f7d822bbbff93ef06501b709900d0bf
tb <toni.blaslov@gmail.com>
2016-12-16 00:03:43 +0000

    added another graph initialization

--- iris.py
+++ iris.py
@@ -63,3 +63,44 @@ sess.close()
 
 
 
+
+#%%
+
+with tf.Graph().as_default():
+    
+    # Create the model
+    x = tf.placeholder(tf.float32, [None, training_data.shape[1]])
+    W = tf.Variable(tf.zeros([training_data.shape[1], training_labels.shape[1]]))
+    b = tf.Variable(tf.zeros([training_labels.shape[1]]))
+    y = tf.matmul(x, W) + b
+
+    # Define loss and optimizer
+    y_ = tf.placeholder(tf.float32, [None, training_labels.shape[1]])
+
+    # define goal function and the optimizer
+    cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y, y_))
+    train_step = tf.train.GradientDescentOptimizer(.05).minimize(cross_entropy)
+
+
+    # global variable initializer
+    init = tf.global_variables_initializer()
+    
+    # create a session and run session to initialize variables
+    with tf.Session() as sess:
+        sess.run(init)
+        
+        # Train
+        tf.global_variables_initializer().run()
+        for i in range(300):
+            sess.run(train_step, feed_dict={x: training_data, y_: training_labels})
+        
+            if i % 100 == 0:
+                # Test trained model
+                correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+                accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+                
+                print("It. %3d" %i,  "   Prediction accurary: ", sess.run(accuracy, feed_dict={x: training_data,
+                                                                             y_: training_labels}))
+
+
+
