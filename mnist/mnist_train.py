8a56bebdee1d3cd5596abcb1623fcdec8e11c6ef
tb <toni.blaslov@gmail.com>
2017-01-16 23:48:49 +0000

    small bugfux

--- mnist/mnist_train.py
+++ mnist/mnist_train.py
@@ -201,7 +201,7 @@ def loss(y, y_):
     Returns:
         Softmax cross entropy tensor.
     """
-    cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y_conv, y_), name='xentropy_mean')
+    cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y, y_), name='xentropy_mean')
     
     return cross_entropy
 
